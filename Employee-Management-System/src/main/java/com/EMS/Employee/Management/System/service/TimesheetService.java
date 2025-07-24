594748253dfcbeb12f02454977435923ff45b339
Shehara Gallage <sheharapgn.22@uom.lk>
2025-07-24 19:05:39 +0530

    feat- last commit

--- Employee-Management-System/src/main/java/com/EMS/Employee/Management/System/service/TimesheetService.java
+++ Employee-Management-System/src/main/java/com/EMS/Employee/Management/System/service/TimesheetService.java
@@ -5,8 +5,14 @@ import java.util.List;
 
 public interface TimesheetService {
     TimesheetDTO createTimesheet(TimesheetDTO dto);
+
     List<TimesheetDTO> getAllTimesheets();
+
     List<TimesheetDTO> getTimesheetsByUserId(Long userId);
-    void deleteTimesheet(Long id, Long userId);
+
+    void deleteTimesheet(Long id);
+
     TimesheetDTO updateStatus(Long id, String status);
-} 
+
+    TimesheetDTO updateTimesheet(Long id, TimesheetDTO dto);
+}
